6b5aa5c6d8b45663797e02dccadb9ddc75dab655
simpleclicks <sheetalparanjpe@gmail.com>
2014-07-21 18:16:20 -0700

    src destination dynamic mapping

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -70,7 +70,8 @@ angular.module('starter.controllers', [])
     })
 
     .controller('MapCtrl', function($scope, $ionicLoading, $compile, LocationService) {
-
+        $scope.cssDestDiv = true;
+        $scope.cssSrcDiv = false;
         $scope.markers = [];
         $scope.map = {
             center: {
@@ -88,41 +89,160 @@ angular.module('starter.controllers', [])
 
         };
 
+        function srcCustomMarker(latlng, map, imageSrc) {
+            this.latlng_ = latlng;
+            this.imageSrc = imageSrc;
+            // Once the LatLng and text are set, add the overlay to the map.  This will
+            // trigger a call to panes_changed which should in turn call draw.
+            this.setMap(map);
+        }
+
+        srcCustomMarker.prototype = new google.maps.OverlayView();
+
+        srcCustomMarker.prototype.draw = function () {
+            // Check if the div has been created.
+            var div = this.div_;
+            if (!div) {
+                // Create a overlay text DIV
+                div = this.div_ = document.createElement('div');
+                // Create the DIV representing our CustomMarker
+
+                div.className = "srcCustomMarker";
+                console.log(div.className);
+
+                var img = document.createElement("img");
+                img.src = this.imageSrc;
+                div.appendChild(img);
+                google.maps.event.addDomListener(div,"click",function(event){
+                    //alert("clicked");
+                });
+
+                // Then add the overlay to the DOM
+                var panes = this.getPanes();
+                panes.overlayImage.appendChild(div);
+            }
+
+            // Position the overlay
+            var point = this.getProjection().fromLatLngToDivPixel(this.latlng_);
+            if (point) {
+                div.style.left = point.x + 'px';
+                div.style.top = point.y + 'px';
+            }
+        };
+
+        srcCustomMarker.prototype.remove = function () {
+            // Check if the overlay was on the map and needs to be removed.
+            if (this.div_) {
+                this.div_.parentNode.removeChild(this.div_);
+                this.div_ = null;
+            }
+        };
+
+        srcCustomMarker.prototype.getPosition = function () {
+            return this.latlng_;
+        };
+
+        function destCustomMarker(latlng, map, imageSrc) {
+            this.latlng_ = latlng;
+            this.imageSrc = imageSrc;
+            // Once the LatLng and text are set, add the overlay to the map.  This will
+            // trigger a call to panes_changed which should in turn call draw.
+            this.setMap(map);
+        }
+
+        destCustomMarker.prototype = new google.maps.OverlayView();
+
+        destCustomMarker.prototype.draw = function () {
+            // Check if the div has been created.
+            var div = this.div_;
+            if (!div) {
+                // Create a overlay text DIV
+                div = this.div_ = document.createElement('div');
+                // Create the DIV representing our CustomMarker
+                div.className = "destCustomMarker";
+                console.log(div.className);
+
+                var img = document.createElement("img");
+                img.src = this.imageSrc;
+                div.appendChild(img);
+                google.maps.event.addDomListener(div,"click",function(event){
+                    //alert("clicked");
+                });
+
+                // Then add the overlay to the DOM
+                var panes = this.getPanes();
+                panes.overlayImage.appendChild(div);
+            }
+
+            // Position the overlay
+            var point = this.getProjection().fromLatLngToDivPixel(this.latlng_);
+            if (point) {
+                div.style.left = point.x + 'px';
+                div.style.top = point.y + 'px';
+            }
+        };
+
+        destCustomMarker.prototype.remove = function () {
+            // Check if the overlay was on the map and needs to be removed.
+            if (this.div_) {
+                this.div_.parentNode.removeChild(this.div_);
+                this.div_ = null;
+            }
+        };
+
+        destCustomMarker.prototype.getPosition = function () {
+            return this.latlng_;
+        };
+
+        var markerData = []
+
+        $scope.loading = $ionicLoading.show({
+            content: 'Getting current location...',
+            showBackdrop: false
+        });
+
+        LocationService.getLatLong().then(
+            function(latLong) {
+                $scope.latLong = latLong;
+                console.log('LatLong=');
+                console.log($scope.latLong);
+
+                //$scope.map.setCenter(new google.maps.LatLng(latLong.lat, latLong.long));
+                $scope.map.center.latitude = $scope.latLong.lat;
+                $scope.map.center.longitude = $scope.latLong.long;
+                $scope.map.zoom = 14;
+
+                console.log("my lat lng" + $scope.map.center.latitude +" " + $scope.map.center.longitude);
+
+                markerData.push({srcpos:[$scope.latLong.lat,$scope.latLong.long],destpos:[37.322619,-121.975555],profileImage:"img/1.jpg"});
+
+                //console.log(JSON.stringify(markerData));
+
+
+
+                for(var i=0;i<markerData.length;i++){
+                    $scope.cssDestDiv = true;
+                    new destCustomMarker(new google.maps.LatLng(markerData[i].destpos[0],markerData[i].destpos[1]), $scope.mapInstance,  markerData[i].profileImage);
+                }
+
+                for(var i=0;i<markerData.length;i++){
+                    $scope.cssDestDiv = false;
+                    new srcCustomMarker(new google.maps.LatLng(markerData[i].srcpos[0],markerData[i].srcpos[1]), $scope.mapInstance,  markerData[i].profileImage);
+
+                }
+
+                $scope.$apply;
+                $ionicLoading.hide();
+
+            },
+
+            function(error) {
+                alert(error);
+            }
+        )
+
         $scope.centerOnMe = function() {
 
-            $scope.loading = $ionicLoading.show({
-                content: 'Getting current location...',
-                showBackdrop: false
-            });
 
-            LocationService.getLatLong().then(
-                function(latLong) {
-                    $scope.latLong = latLong;
-                    console.log('LatLong=');
-                    console.log($scope.latLong);
-
-                    //$scope.map.setCenter(new google.maps.LatLng(latLong.lat, latLong.long));
-                    $scope.map.center.latitude = $scope.latLong.lat;
-                    $scope.map.center.longitude = $scope.latLong.long;
-                    $scope.map.zoom = 14;
-
-                    console.log("my lat lng" + $scope.map.center.latitude +" " + $scope.map.center.longitude);
-
-                    marker = new google.maps.Marker({
-                        position: {lat: $scope.latLong.lat, lng: $scope.latLong.long },
-                        map: $scope.mapInstance
-                    });
-
-                    //$scope.map = map;
-
-                    $scope.$apply;
-                    $ionicLoading.hide();
-
-                },
-
-                function(error) {
-                    alert(error);
-                }
-            )
         };
     });
